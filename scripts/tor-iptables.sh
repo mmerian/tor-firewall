2f471911b4916edaae0497bbf8cc8ba021d40a1c
Maxime Mérian <maxime@merian.me>
2016-01-24 15:02:43 +0100

    Removed i2p support, drop udp and icmp

--- scripts/tor-iptables.sh
+++ scripts/tor-iptables.sh
@@ -16,12 +16,6 @@ if [ -z "$_tor_uid" ]; then
     $_tor_uid=tor
 fi
 
-# i2p user id
-_i2p_uid=`cat /etc/passwd|grep i2p|cut -f 1 -d :`
-if [ -z "$_i2p_uid" ]; then
-    $_i2p_uid=i2p
-fi
-
 _trans_port=9040
 
 # Destinations that will not
@@ -53,14 +47,8 @@ $IPT -A INPUT -i lo -j ACCEPT
 # Allow all tor traffic
 $IPT -t nat -A OUTPUT -m owner --uid-owner $_tor_uid -j RETURN
 
-# Allow all i2p traffic
-$IPT -t nat -A OUTPUT -m owner --uid-owner $_i2p_uid -j RETURN
-
 # Route DNS queries through tor
 $IPT -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 53
-
-# Allow ICMP requests, they are not routed by tor
-$IPT -t nat -A OUTPUT -p icmp -j RETURN
 
 # Allow direct destinations
 for i in $_non_tor; do
@@ -68,14 +56,8 @@ for i in $_non_tor; do
     $IPT -A OUTPUT -d $i -j RETURN
 done
 
-# Allow UDP, since tor can't handle it
-$IPT -t nat -A OUTPUT -p udp -j ACCEPT
-
 # Route all traffic through tor
 $IPT -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $_trans_port
-
-# Allow ICMP requests
-$IPT -A OUTPUT -p icmp -j RETURN
 
 $IPT -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j LOG --log-prefix "Transproxy leak blocked: " --log-uid
 $IPT -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j LOG --log-prefix "Transproxy leak blocked: " --log-uid
@@ -83,7 +65,5 @@ $IPT -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags A
 $IPT -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
 
 $IPT -A OUTPUT -m owner --uid-owner $_tor_uid -j ACCEPT
-$IPT -A OUTPUT -m owner --uid-owner $_i2p_uid -j ACCEPT
 
-$IPT -A OUTPUT -p udp -j ACCEPT
 $IPT -A OUTPUT -j REJECT
